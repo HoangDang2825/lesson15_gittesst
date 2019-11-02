6a19aa4ee4d1ec7d0daaeac7e249c1a9cf0e7b0c
Hieu Truong <44727643+HieuTruong215@users.noreply.github.com>
2019-11-02 22:36:01 +0700

    CM03 - Ex01

--- src/test/Ex01.java
+++ src/test/Ex01.java
@@ -5,22 +5,27 @@ import java.util.function.BiFunction;
 public class Ex01 {
 	public static void main(String[] args) {
 		int a = 2, b = 3;
-		System.out.println("Tong 2 so a b: "+sum(a, b));
-		System.out.println("Hieu 2 so a b: "+sub(a, b));
+		System.out.println("Tong 2 so a b: " + sum(a, b));
+		System.out.println("Hieu 2 so a b: " + sub(a, b));
+		System.out.println("Tich 2 so a va b: " + tich(a, b));
 		lambda();
 	}
-	
+
 	private static int sum(int a, int b) {
 		return a + b;
 	}
-	
+
 	private static int sub(int a, int b) {
 		return a - b;
 	}
+
 	private static void lambda() {
-		BiFunction<Integer, Integer, Integer> sum = ( a,  b) -> a + b;
+		BiFunction<Integer, Integer, Integer> sum = (a, b) -> a + b;
 		BiFunction<Integer, Integer, Integer> sup = (a, b) -> a - b;
 		BiFunction<Integer, Integer, Integer> dev = (a, b) -> a * b;
 		BiFunction<Integer, Integer, Integer> mul = (a, b) -> a / b;
+	}
+	private static int tich(int a, int b) {
+		return a / b;
 	}
 }
